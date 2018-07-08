17349606ef7b603cf29ae84ab2c923977f9f4949
clairethebear <claire.oubelaid@gmail.com>
2018-07-08 19:35:20 +1000

    Fuel Consumption Application
    An application that takes three float variables. Cost, Distance Travelled and Litres used.
    
    The application calculates the Kilometeres per Kilometer. And the cost of the distance travelled based on the cost entered per litre.
    
    Notes:
    Can add functionality into the application to switch between KM or Miles. Also can add functionality to change the currency for the cost.

---
+++ Fuel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace FuelClass
+{
+    public class Fuel
+    {
+        private float _distance_travelled;
+        private float _cost;
+        private float _litres_used;
+
+        public Fuel(float distance, float cost, float litres_used)
+        {
+            _distance_travelled = distance;
+            _cost = cost;
+            _litres_used = litres_used;
+        }
+
+        public Fuel()
+        {
+            _distance_travelled = 0;
+            _cost = 0;
+            _litres_used = 0;
+        }
+
+        public float DistanceTravelled
+        {
+            set => _distance_travelled = value;
+            get
+            {
+                return _distance_travelled;
+            }
+        }
+        public float Cost
+        {
+            set => _cost = value;
+            get
+            {
+                return _cost;
+            }
+        }
+        public float LitresUsed
+        {
+            set => _litres_used = value;
+            get
+            {
+                return _litres_used;
+            }
+        }
+
+        public float KilometersPerLitre()
+        {
+            return (_litres_used * 100) / _distance_travelled;
+        }
+
+        public float CostForDistanceTravelled()
+        {
+            return _litres_used * _cost;
+        }
+
+        public string CheckUserInput(string value)
+        {
+            /* Dave Question: Is this the best way to implement a Try catch? */
+            int line;
+            while (!int.TryParse(value, out line))
+            {
+                Console.WriteLine("Invalid input. Please enter 1, 2, or 3.");
+                value = Console.ReadLine();
+            }
+            return value;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FuelClass;
+
+namespace fuel_usage_app
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Fuel Fuel = new Fuel();
+
+            /* Dave Question: what is the most C# way of splitting writelines?*/
+            Console.WriteLine("Please enter the distance travelled in Kilometers:");
+            string distance_travelled = Console.ReadLine();
+            distance_travelled = Fuel.CheckUserInput(distance_travelled);
+
+            Console.WriteLine("Please enter the litres of petrol used:");
+            string litres_used = Console.ReadLine();
+            litres_used = Fuel.CheckUserInput(litres_used);
+
+            Console.WriteLine("Please enter the cost of fuel per litre in Australian Dollarydoo's (example 1$ per litre):");
+            string cost = Console.ReadLine();
+            cost = Fuel.CheckUserInput(cost);
+
+            /* Parse the userinput string into a float variable. */
+            Fuel.DistanceTravelled = float.Parse(
+                distance_travelled, 
+                System.Globalization.CultureInfo.InvariantCulture);
+            Fuel.Cost = float.Parse(
+                cost, System.Globalization.CultureInfo.InvariantCulture);
+            Fuel.LitresUsed = float.Parse(
+                litres_used, System.Globalization.CultureInfo.InvariantCulture);
+
+            Console.Write("{0} litres per 100KM.\n", 
+                          Fuel.KilometersPerLitre().ToString());
+            Console.Write("${0} spent on {1} KM of travel.", 
+                          Fuel.CostForDistanceTravelled().ToString(), 
+                          Fuel.DistanceTravelled);
+        }
+
+    }
+}
